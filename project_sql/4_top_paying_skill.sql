44463611b6b443cc634bbe4654aa3fcb2c7c68b0
okolie awele <aweleokolie77@gmail.com>
2025-05-14 13:31:54 -0400

    Initial commit

---
+++ project_sql/4_top_paying_skill.sql
@@ -0,0 +1,33 @@
+/*What are the top skills based on salary?
+- Look at the average salary associated with each skill
+- Focuses on roles with specified salaries, regardless of location
+- Why? It reveals how different skills impact salary levels for data analysts and helps identify the most financially rewarding skills to acquire or improve
+*/
+
+SELECT 
+    skills,
+    ROUND(AVG(salary_year_avg), 0) as avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL 
+    AND job_work_from_home = TRUE
+GROUP BY 
+    skills
+ORDER BY 
+    avg_salary DESC
+LIMIT 25
+
+/*Top Paying Tech Skills – Quick Insights
+Big Data tools pay most: PySpark ($208K), Databricks, and Airflow show high demand in data engineering.
+
+Python data stack is strong: Pandas, NumPy, Jupyter, Scikit-learn average $125K–$152K.
+
+Cloud & DevOps matter: Tools like Kubernetes, GCP, GitLab, Jenkins offer high salaries ($122K+).
+
+AutoML tools pay well: DataRobot and Watson earn ~$160K.
+
+Niche = $$$: Less common tools (e.g., Couchbase, MicroStrategy) offer high salaries due to low supply.
+*/
